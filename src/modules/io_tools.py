2841edc82132a0d61b9af0c64796b5bd64a10ea0
noresources <dennis.petta@gmail.com>
2019-04-16 17:08:06 +0300

    Feature segmentation and addition to the dataset

--- src/modules/io_tools.py
+++ src/modules/io_tools.py
@@ -1,37 +1,98 @@
 from scipy.io import wavfile
 import pandas as pd
-import os
+import os, csv, math
+import seaborn as sns
+from pathlib import Path
+from numpy import genfromtxt
+import matplotlib.pyplot as plot
+import matplotlib.pylab as plt
 
-dataset_folder = 'INPUT_DATASET_FOLDER'
-pickle_folder = 'OUTPUT_DATASET_FOLDER'
+
+script_path = Path(os.path.realpath(__file__))
+project_path = script_path.parent.parent.parent
+
+dataset_folder = project_path / Path('assets/Whole')
+pickle_folder = project_path / Path('assets/Pickles')
 
 
-def create_dataset(path):
-    annotations = pd.read_csv(path,
+
+
+def create_dataset(target_path):
+    '''
+    This function creates the dataset for a given path of inputs
+    :param path:
+    :return: dataset and annotation
+    '''
+    annotations = pd.read_csv( target_path,
                               quotechar='"', skipinitialspace=True, names=['Filename', 'Label', 'begin', 'end'])
 
     filenames = set([annotation[0] for idx, annotation in annotations.iterrows()])
 
-    dataset = pd.DataFrame(columns=['Filename', 'Label', 'Begin', 'End', 'Sample'])
+    dataset = pd.DataFrame(columns=['Filename', 'Label', 'Begin', 'End', 'Sample', 'CEPST','SPECT', 'MFCC', 'CWT', 'ZCR'])
 
     for idx, filename in enumerate(filenames):
         # Read the .wav file
-        fs, data = wavfile.read(os.path.join(dataset_folder, filename))
+        fs, data = wavfile.read(dataset_folder / filename)
+
+        main_name = Path(filename.split('.')[0])
+
+        # Read the feature files
+        dat_cepst = genfromtxt( str(dataset_folder / main_name) + '_cepst.csv', delimiter=',')
+        dat_spect = genfromtxt( str(dataset_folder / main_name) + '_spect.csv', delimiter=',')
+        dat_mfcc = genfromtxt( str(dataset_folder / main_name) + '_mfcc.csv', delimiter=',')
+        dat_cwt = genfromtxt( str(dataset_folder / main_name) + '_cwt.csv', delimiter=',')
+        dat_zcr = genfromtxt( str(dataset_folder / main_name) + '_zcr.csv', delimiter=',')
+
+        # plot.subplot()
+        # # plot.plot(dat_cepst)
+        # ax = sns.heatmap(dat_spect)
+        # plot.xlabel('Time')
+        # plot.ylabel('Frequency')
+        # plot.show()
+
+
         # Get the relevant entries from the dataset
         relevant = annotations.loc[annotations['Filename'] == filename]
 
         # Extract and append the entries with the audio snippet
         for idx, snippet in relevant.iterrows():
             begin, end = (snippet[2], snippet[3])
+
+            q_begin, q_end = (math.floor(begin/128) , math.ceil( end/128) )
+
+            # # Verification
+            # print('Label: ',snippet[1], ' Length: ', len(data), ' Quantas 778 : ', len(data)/778,
+            #       ' Quantas 128 : ', len(data)/128, ' Quant: {0} - {1}'.format(math.floor(begin/128),math.ceil( end/128)) )
+
+            # # Plot the sample
+            # plot.subplot()
+            # # plot.plot(dat_cepst)
+            # ax = sns.heatmap(dat_spect[:,math.floor(begin/128) : math.ceil( end/128) ])
+            # plot.xlabel('Time')
+            # plot.ylabel('Frequency')
+            # plot.show()
+
+
             row_dict = {'Filename': snippet[0], 'Label': snippet[1], 'Begin': begin, 'End': end,
-                        'Sample': [data[begin:end]]}
+                        'Sample': [data[begin:end]],
+                        'CEPST': [dat_cepst[: ,q_begin: q_end]],
+                        'SPECT': [dat_spect[:, q_begin: q_end]],
+                        'MFCC': [dat_mfcc[:, q_begin: q_end]],
+                        'CWT': [dat_cwt[:, q_begin: q_end]],
+                        'ZCR': [dat_zcr[ q_begin: q_end]]
+                        }
             dataset.loc[len(dataset)] = row_dict
 
         print(idx)
 
-    # Save the dataset to memory
-    annotations.to_pickle(os.path.join(pickle_folder, 'annotation_whole.pkl'))  # where to save it, usually as a .pkl
-    dataset.to_pickle(os.path.join(pickle_folder, 'dataset_whole.pkl'))
+    return annotations, dataset
+
+
+
+annotations, dataset = create_dataset(dataset_folder/ 'annotation_whole_dei.csv')
+
+annotations.to_pickle(os.path.join(pickle_folder, 'annotation_whole.pkl'))  # where to save it, usually as a .pkl
+dataset.to_pickle(os.path.join(pickle_folder, 'dataset_whole.pkl'))
 
 # How to read
 
